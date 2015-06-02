25deb48a901e5241747118987cbf12ba29a423e5
Hitesh Joshi <me@hiteshjoshi.com>
2015-06-02 13:32:36 +0530

    updating examples

--- examples/sentiments.js
+++ examples/sentiments.js
@@ -14,7 +14,7 @@ var server = http.createServer(function (request, response) {
   var queryData = url.parse(request.url, true).query;
   response.writeHead(200, {"Content-Type": "text/plain"});
 
-  if (queryData.name) {
+  if (queryData.q) {
     
     coreNLP.process(queryData.q, function(err, result) {
 	    if(err)
