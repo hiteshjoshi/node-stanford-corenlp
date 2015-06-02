e98ea503b4178b98aa3a66910bda1e71fbc34aa0
Hitesh Joshi <me@hiteshjoshi.com>
2015-06-02 13:27:34 +0530

    updating examples

--- examples/sentiments.js
+++ examples/sentiments.js
@@ -6,6 +6,27 @@ var config = {'nlpPath':path.join ( __dirname,'./../corenlp'),'version':'3.5.2'}
 var coreNLP = new NLP.StanfordNLP(config);
 coreNLP.loadPipelineSync();
 
-coreNLP.process('This is so good.', function(err, result) {
-    console.log(err,JSON.stringify(result));
+
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (request, response) {
+  var queryData = url.parse(request.url, true).query;
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  if (queryData.name) {
+    
+    coreNLP.process(queryData, function(err, result) {
+	    if(err)
+	    	throw err;
+	    else
+	    	response.end(JSON.stringify(result));
+	});
+    
+
+  } else {
+    response.end("Hello standford-coreNLP!\n");
+  }
 });
+
+server.listen(8000);
