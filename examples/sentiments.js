e36ad617539a1cccf85f15884d5f8cbb80d71eee
Hitesh <me@hiteshjoshi.com>
2015-07-29 01:30:41 +0530

    more examples

--- examples/sentiments.js
+++ examples/sentiments.js
@@ -5,7 +5,7 @@ var config = {
 
 	'nlpPath':path.join ( __dirname,'./../corenlp'), //the path of corenlp
 	'version':'3.5.2', //what version of corenlp are you using
-	'annotators': ['tokenize','ssplit','pos','parse','sentiment','depparse'], //optional!
+	'annotators': ['tokenize','ssplit','pos','parse','sentiment','depparse','quote'], //optional!
 	'extra' : {
       'depparse.extradependencie': 'SUBJ_ONLY'
     }
